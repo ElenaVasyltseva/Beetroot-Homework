b6c417351f5b3660df413d35e90c9771b942839a
ElenaVasyltseva <77539419+ElenaVasyltseva@users.noreply.github.com>
2021-05-18 18:28:17 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,26 @@
+
+#hello, I am here
+
+#here
+
+#and here!
+print('hello', end='')
+print(',', end=' ')
+print('I', end=' ')
+print('am', end=' ')
+print('here', end='\n\n')
+print('here', end='\n\n')
+print('and', end=' ')
+print('here', end='!\n\n')
+
+# 1 --> 2 --> 3 --> 4 --> 5
+print(1, 2, 3, 4, 5, sep=' --> ')
+print(end='\n\n')
+
+# olena_vasyltseva@gmail.com
+print('olena', end='')
+print('_', end='')
+print('vasyltseva',  end='')
+print('@', end='')
+print('gmail', 'com', sep='.')
+
