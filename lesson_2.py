ceb2414f499c1571b9e9963569ab5b9e9f24a1f5
ElenaVasyltseva <77539419+ElenaVasyltseva@users.noreply.github.com>
2021-05-19 21:54:02 +0300

    Add files via upload

---
+++ lesson_2.py
@@ -0,0 +1,31 @@
+# task1
+
+name = 'Elena'
+day = 'Wednesday'
+
+print(f'Good day {name}! {day} is a perfect day to learn some python.')
+
+print('Good day {name}! {day} is a perfect day to learn some python.'.format(name=name, day=day))
+
+print('Good day {}! {} is a perfect day to learn some python.'.format(name, day))
+
+print('Good day %s! %s is a perfect day to learn some python.' % (name, day))
+
+# task2
+
+first_name = 'Elena'
+last_name = 'Vasyltseva'
+print('Hello' + ' ' + first_name + ' ' + last_name)
+
+# task3
+
+a = 10
+b = 8
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a % b)
+print(a//b)
