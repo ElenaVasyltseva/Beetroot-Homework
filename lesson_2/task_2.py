f9cf0af0c91cb5d273bf097fb82fbccbdb336fff
ElenaVasyltseva <77539419+ElenaVasyltseva@users.noreply.github.com>
2021-05-22 11:10:56 +0300

    Add files via upload

---
+++ lesson_2/task_2.py
@@ -0,0 +1,8 @@
+#  task2
+#  Manipulate strings.
+#  Save your first and last name as separate variables, then use string concatenation to add them together
+#  with a white space in between and print a greeting.
+
+first_name = 'Elena'
+last_name = 'Vasyltseva'
+print('Hello' + ' ' + first_name + ' ' + last_name)
---
+++ lesson_2/task_3.py
@@ -0,0 +1,15 @@
+#  task3
+#  Using python as a calculator.
+#  Make a program with 2 numbers saved in separate variables a and b, then print the result for each of the following:
+#  Addition, Subtraction, Division, Multiplication, Exponent (Power), ModulusFloor,  division
+
+a = 10
+b = 8
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a % b)
+print(a//b)
