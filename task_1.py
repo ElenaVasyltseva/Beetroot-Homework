7d699376578f6f813f7bd18e6931a337ce091788
ElenaVasyltseva <77539419+ElenaVasyltseva@users.noreply.github.com>
2021-06-01 19:27:29 +0300

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,10 @@
+#  Task 1
+#  A simple function.
+#  Create a simple function called favorite_movie,
+#  which takes a string containing the name of your favorite movie.
+#  The function should then print “My favorite movie is named {name}”.
+
+def favorite_movie(movie_title):
+    print(f'My favorite movie is named "{movie_title}".')
+    
+favorite_movie('Green book')
